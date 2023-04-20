729c95cd9d9564ff5e4a769d9f5f799cadbcd72b
Josh Eilers <eilers.joshua@gmail.com>
2023-04-20 16:58:31 -0700

    center vertical movements

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,7 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<Tab>', '<CMD>Neotree focus<CR>')
+vim.keymap.set('n', '<C-f>', '<C-f>zz')
+vim.keymap.set('n', '<C-b>', '<C-b>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
