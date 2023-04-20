e9b155316a9646287d03bd99a4ef6bf1bc9476f8
Josh Eilers <eilers.joshua@gmail.com>
2023-04-20 16:45:40 -0700

    get neotree filtering working again

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -12,11 +12,7 @@ return {
   config = function ()
     require('neo-tree').setup {
       filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        }
+        follow_current_file = true,
       }
     }
   end,
