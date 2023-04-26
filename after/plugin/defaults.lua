34f2a3af5b43c43a3f821ac4edce68a9ace23b85
Josh Eilers <eilers.joshua@gmail.com>
2023-04-26 15:17:58 -0700

    keybindings update

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,16 +1,27 @@
+-- options
+--
 vim.opt.relativenumber = true
 
-vim.keymap.set('n', '<leader>\\', '<cmd>Neotree toggle<cr>')
+-- file tree
+
+vim.keymap.set('n', '<tab>', '<cmd>Neotree toggle<cr>')
+
+-- enhance vertical navigation
 
 vim.keymap.set('n', '<c-f>', '<c-f>zz')
 vim.keymap.set('n', '<c-b>', '<c-b>zz')
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
 vim.keymap.set('n', '<c-u>', '<c-u>zz')
 
-vim.keymap.set('n', '-', '<c-w>s<c-w><right>')
-vim.keymap.set('n', '\\', '<c-w>v<c-w><down>')
+-- splits
+
+vim.keymap.set('n', '<leader>-', '<c-w>s<c-w>j')
+vim.keymap.set('n', '<leader>\\', '<c-w>v<c-w>l')
+
+-- window navigation
 
 vim.keymap.set('n', '<leader>j', '<c-w>j')
 vim.keymap.set('n', '<leader>k', '<c-w>k')
 vim.keymap.set('n', '<leader>h', '<c-w>h')
 vim.keymap.set('n', '<leader>l', '<c-w>l')
+
